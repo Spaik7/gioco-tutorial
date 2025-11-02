5a700614f2fd0c755d8fec1d56851cdb92b33b9c
SimoneChiodo <simochi.005@gmail.com>
2025-11-02 19:37:56 +0100

    Windows export

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	queue_free()  # Remove coin from scene
+	game_manager.add_point()
+	animation_player.play("pickup")  # Remove coin from scene
